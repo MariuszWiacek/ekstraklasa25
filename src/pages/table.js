44e80cb07171612a57379f2dee6f7aaa9a652ff4
MariuszLillup <Mariusz@lillup.com>
2024-12-03 17:47:39 +0000

    hidden firebase

--- src/pages/table.js
+++ src/pages/table.js
@@ -6,15 +6,16 @@ import Stats from './stats';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAEUAgb7dUt7ZO8S5-B4P3p1fHMJ_LqdPc",
-  authDomain: "polskibet-71ef6.firebaseapp.com",
-  databaseURL: "https://polskibet-71ef6-default-rtdb.europe-west1.firebasedatabase.app/",
-  projectId: "polskibet-71ef6",
-  storageBucket: "polskibet-71ef6.appspot.com",
-  messagingSenderId: "185818867502",
-  appId: "1:185818867502:web:b582993ede95b06f80bcbf",
-  measurementId: "G-VRP9QW7LRN"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 
 const firebaseApp = initializeApp(firebaseConfig);
 const database = getDatabase(firebaseApp);
