63db3e441f89563e9c3bb4c1aab58f537001f94f
MariuszLillup <Mariusz@lillup.com>
2025-01-22 07:41:29 +0000

    bar

--- src/pages/poll.js
+++ src/pages/poll.js
@@ -85,6 +85,14 @@ const Poll = ({ onClose }) => {
     return <div style={{ color: "white" }}>Ładowanie ankiety...</div>;
   }
 
+  // Calculate the total number of votes
+  const totalVotes = votes.option1.count + votes.option2.count + votes.oldRules.count;
+
+  // Calculate the percentage for each option
+  const option1Percentage = totalVotes ? (votes.option1.count / totalVotes) * 100 : 0;
+  const option2Percentage = totalVotes ? (votes.option2.count / totalVotes) * 100 : 0;
+  const oldRulesPercentage = totalVotes ? (votes.oldRules.count / totalVotes) * 100 : 0;
+
   return (
     <div style={pollContainerStyle}>
       <div style={pollContentStyle}>
@@ -108,7 +116,7 @@ const Poll = ({ onClose }) => {
           />
           <div>
             <label style={optionLabelStyle}>
-            <hr></hr><input
+              <hr></hr><input
                 type="radio"
                 value="Option 1"
                 checked={selectedOption === "Option 1"}
@@ -117,6 +125,14 @@ const Poll = ({ onClose }) => {
               1. Bonus 10 zł - za każdą wygraną kolejkę – jeśli kolejkę wygra więcej osób, pula przechodzi na następną rundę.
             </label>
             <p style={voteCountStyle}>Głosy: {votes.option1.count}</p>
+            <div style={progressBarContainerStyle}>
+              <div
+                style={{
+                  ...progressBarStyle,
+                  width: `${option1Percentage}%`, // Set width dynamically
+                }}
+              ></div>
+            </div>
             <div style={votersStyle}>
               {(votes.option1.voters || []).map((voter, index) => (
                 <span key={index}>{voter} </span>
@@ -125,7 +141,7 @@ const Poll = ({ onClose }) => {
           </div>
           <div>
             <label style={optionLabelStyle}>
-            <hr></hr> <input
+              <hr></hr> <input
                 type="radio"
                 value="Option 2"
                 checked={selectedOption === "Option 2"}
@@ -134,6 +150,14 @@ const Poll = ({ onClose }) => {
               2. Bonus 50 zł dla najlepszego typera miesiąca (4 kolejki).
             </label>
             <p style={voteCountStyle}>Głosy: {votes.option2.count}</p>
+            <div style={progressBarContainerStyle}>
+              <div
+                style={{
+                  ...progressBarStyle,
+                  width: `${option2Percentage}%`, // Set width dynamically
+                }}
+              ></div>
+            </div>
             <div style={votersStyle}>
               {(votes.option2.voters || []).map((voter, index) => (
                 <span key={index}>{voter} </span>
@@ -151,6 +175,19 @@ const Poll = ({ onClose }) => {
               Stare zasady - bez dodatkowych bonusów
             </label>
             <p style={voteCountStyle}>Głosy: {votes.oldRules.count}</p>
+            <div style={progressBarContainerStyle}>
+              <div
+                style={{
+                  ...progressBarStyle,
+                  width: `${oldRulesPercentage}%`, // Set width dynamically
+                }}
+              ></div>
+               <div style={votersStyle}>
+              {(votes.oldRules.voters || []).map((voter, index) => (
+                <span key={index}>{voter} </span>
+              ))}
+            </div>
+            </div>
           </div>
           <button type="submit" style={submitButtonStyle}>
             Wyślij
@@ -176,23 +213,22 @@ const pollContainerStyle = {
 };
 
 const pollContentStyle = {
-    backgroundColor: "white",
-    borderRadius: "10px",
-    padding: "20px",
-    width: "90%",
-    maxWidth: "500px",
-    textAlign: "center",
-    position: "relative",
-    maxHeight: "80%", // Limit the height to allow for scrolling
-    overflowY: "auto", // Enable vertical scrolling
-  };
-  
+  backgroundColor: "white",
+  borderRadius: "10px",
+  padding: "20px",
+  width: "90%",
+  maxWidth: "500px",
+  textAlign: "center",
+  position: "relative",
+  maxHeight: "80%", // Limit the height to allow for scrolling
+  overflowY: "auto", // Enable vertical scrolling
+};
 
 const votersStyle = {
-    fontSize: "12px",
-    color: "gray",
-    marginBottom: "10px",
-  };
+  fontSize: "12px",
+  color: "gray",
+  marginBottom: "10px",
+};
 
 const closeButtonStyle = {
   position: "absolute",
@@ -224,6 +260,20 @@ const optionLabelStyle = {
 const voteCountStyle = {
   fontSize: "14px",
   color: "black",
+};
+
+const progressBarContainerStyle = {
+  width: "100%",
+  height: "10px",
+  backgroundColor: "#ccc",
+  borderRadius: "5px",
+  marginBottom: "10px",
+};
+
+const progressBarStyle = {
+  height: "100%",
+  backgroundColor: "green",
+  borderRadius: "5px",
 };
 
 const submitButtonStyle = {
